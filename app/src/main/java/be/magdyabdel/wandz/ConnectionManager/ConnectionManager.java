9d15ca44c70faee9f37dadefa628f740f46ace79
Simon <simoncools@hotmail.be>
2019-11-26 20:32:23 +0100

    Added connection manager

---
+++ app/src/main/java/be/magdyabdel/wandz/ConnectionManager/ConnectionManager.java
@@ -0,0 +1,66 @@
+package be.magdyabdel.wandz.ConnectionManager;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+
+public class ConnectionManager {
+
+    private String ip;
+    private int port;
+    Socket socket;
+
+    public ConnectionManager(String ip, int port){
+        this.ip = ip;
+        this.port = port;
+    }
+
+
+    public int connect(){
+        try {
+            socket = new Socket(ip, port);
+        }catch(UnknownHostException uk){
+            return 1; //Host could not be found
+        }catch(IOException e){
+            return 2; //IO error when creating the socket
+        }
+
+        return 0; //No errors, everything ok
+    }
+
+
+
+    public int sendData(String data){
+        if(socket!=null) {
+            try {
+                DataOutputStream outToServer = new DataOutputStream(socket.getOutputStream());
+                outToServer.writeBytes(data + "\n");
+            } catch (IOException e) {
+                return 1; //IO error when sending
+            }
+
+            return 0; //No errors, everything ok
+        }else{
+            return 2; //Socket is null
+        }
+    }
+
+    public ArrayList<String> readAllData(){
+        ArrayList<String> dataList = new ArrayList<>();
+            try {
+                BufferedReader inFromClient = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                while(inFromClient.ready()){
+                    String nextLine = inFromClient.readLine();
+                    dataList.add(nextLine);
+                }
+
+            }catch(IOException e){
+                System.out.println("Error reading data from client.");
+            }
+        return dataList;
+    }
+}
