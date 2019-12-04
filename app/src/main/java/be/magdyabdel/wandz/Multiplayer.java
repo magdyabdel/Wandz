52162c4f36b14f58d1d4a23b5e141f216ba67ca6
magdy.abdel <magdy.abdel@student.kuleuven.be>
2019-12-04 14:34:13 +0100

    Changed theme to fullscreen mode and updated multiplayer design

--- app/src/main/java/be/magdyabdel/wandz/Multiplayer.java
+++ app/src/main/java/be/magdyabdel/wandz/Multiplayer.java
@@ -88,7 +88,26 @@ public class Multiplayer extends AppCompatActivity implements View.OnClickListen
 
     @Override
     public void onWindowFocusChanged (boolean hasFocus){
-        // nothing
+        if (hasFocus) {
+            hideSystemUI();
+        }
+    }
+
+    private void hideSystemUI() {
+        // Enables regular immersive mode.
+        // For "lean back" mode, remove SYSTEM_UI_FLAG_IMMERSIVE.
+        // Or for "sticky immersive," replace it with SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                        // Set the content to appear under the system bars so that the
+                        // content doesn't resize when the system bars hide and show.
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        // Hide the nav bar and status bar
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN);
     }
 
     @Override
@@ -96,6 +115,7 @@ public class Multiplayer extends AppCompatActivity implements View.OnClickListen
         super.onCreate(savedInstanceState);
 
         this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE); // Force landscape mode on create for this activity
+        hideSystemUI();
 
         connectionManager = (ConnectionManager) getIntent().getSerializableExtra("conman");
 
