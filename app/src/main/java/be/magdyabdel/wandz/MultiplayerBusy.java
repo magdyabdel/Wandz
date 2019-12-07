49287d1fcbdac023b8d11cf2ae73afd292d3c6c8
magdy.abdel <magdy.abdel@student.kuleuven.be>
2019-12-07 15:41:14 +0100

    Added new activities

---
+++ app/src/main/java/be/magdyabdel/wandz/MultiplayerBusy.java
@@ -0,0 +1,13 @@
+package be.magdyabdel.wandz;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MultiplayerBusy extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_multiplayer_busy);
+    }
+}
